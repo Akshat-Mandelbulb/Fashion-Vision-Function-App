44f6b83fcaa42114936492bc976d59edff4e1bdb
Akshat Gupta <akshatgupta0030@gmail.com>
2026-02-02 15:43:51 +0530

    feat: Add request logger

--- src/modules/vision_analysis/vision_analysis_routes.py
+++ src/modules/vision_analysis/vision_analysis_routes.py
@@ -1,11 +1,14 @@
 from azure.functions import Blueprint
 
 from modules.vision_analysis.vision_analysis_controller import VisionAnalysisController
+from shared.utils.logger_config import setup_logger
+
+logger = setup_logger()
 
 vision_analysis_bp = Blueprint()
 
 
 @vision_analysis_bp.route(route="analyse-image", methods=["POST"])
 async def analyze_image(req):
-    print("Analyzing image: ", req)
+    logger.info(f"\n==>> req: {req}")
     return await VisionAnalysisController.analyze_image_from_url(req)
