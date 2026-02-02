2ad403db3488d29fbb31eefac147c134e15ae062
Akshat Gupta <akshatgupta0030@gmail.com>
2026-02-02 15:08:48 +0530

    feat: Add analyse image route

--- src/modules/vision_analysis/vision_analysis_routes.py
+++ src/modules/vision_analysis/vision_analysis_routes.py
@@ -1,3 +1,11 @@
 from azure.functions import Blueprint
 
+from modules.vision_analysis.vision_analysis_controller import VisionAnalysisController
+
 vision_analysis_bp = Blueprint()
+
+
+@vision_analysis_bp.route(route="analyse-image", methods=["POST"])
+def analyze_image(req):
+    print("Analyzing image: ", req)
+    return VisionAnalysisController.analyze_image_from_url(req)
